f9337e1438edcacab91c90b1b4d4fe16c764031c
Robert S.R. Myers <me.robmyers@gmail.com>
2020-05-15 14:35:31 +0100

    final commit before total restart

--- model/level/babylon.model.ts
+++ model/level/babylon.model.ts
@@ -31,25 +31,19 @@ export const babylonNavMeshParams: BABYLON.INavMeshParameters = {
 
 export function loadInitialScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
   const scene = new BABYLON.Scene(engine);
+  // scene.clearColor = new BABYLON.Color4(1, 1, 1, 1);
   const sun = new BABYLON.HemisphericLight('sun', new BABYLON.Vector3(1, 1, 0), scene);
   sun.intensity = 0.5;
-
   const spotlight = new BABYLON.SpotLight('spotlight', new BABYLON.Vector3(0, 10, 0), BABYLON.Vector3.Down(), Math.PI/2, 10, scene);
   spotlight.intensity *= 0.2;
-  // const shadowGenerator = new BABYLON.ShadowGenerator(1024, spotlight);
-  // shadowGenerator.bias = 0.00001; // Important
-  // const shadowMap = shadowGenerator.getShadowMap()!;
-  // scene.meshes.forEach(mesh => shadowMap.renderList!.push(mesh));
-  // shadowMap.refreshRate = BABYLON.RenderTargetTexture.REFRESHRATE_RENDER_ONCE;
-
   setupCamera(canvas, scene);
 
   const gridMaterial = new GridMaterial('grid-material', scene);
   gridMaterial.gridRatio = 0.5;
   gridMaterial.gridOffset = new BABYLON.Vector3(-0.5, 0, -0.5);
-  gridMaterial.backFaceCulling = false;
+  gridMaterial.backFaceCulling = true;
   gridMaterial.mainColor = new BABYLON.Color3(1, 1, 1);
-  gridMaterial.lineColor = new BABYLON.Color3(0.3, 0.3, 0.3);
+  gridMaterial.lineColor = new BABYLON.Color3(0.5, 0.5, 0.5);
   
   const white = new BABYLON.BackgroundMaterial('white-material', scene);
   white.useRGBColor = false;
@@ -76,23 +70,22 @@ function setupCamera(canvas: HTMLCanvasElement, scene: BABYLON.Scene) {
 export function createTile(x: number, y: number, scene: BABYLON.Scene) {
   const ground = BABYLON.MeshBuilder.CreateGround(`tile-${x}-${y}`,{ width: 1, height: 1, subdivisions: 2 });
   ground.material = scene.getMaterialByName('grid-material');
-
+  // ground.material = scene.getMaterialByName('white-material');
   ground.position = new BABYLON.Vector3(x + 0.5, 0, y + 0.5);
   scene.addMesh(ground);
   return ground;
 }
 
 export function createWall(u: Vector2, v: Vector2, scene: BABYLON.Scene) {
-  
   const delta = 0.05;
-  const height = 2.5;
+  const height = 2;
   const wall = BABYLON.MeshBuilder.CreateBox(`wall-${u}-${v}`, {
     height,
     width: Math.abs(v.x - u.x) + 2 * delta,
     depth: Math.abs(v.y - u.y) + 2 * delta,
   });
   // wall.receiveShadows = true;
-  // wall.material = scene.getMaterialByName('gridMaterial');
+  // wall.material = scene.getMaterialByName('grid-material');
   // wall.material = scene.getMaterialByName('white-material');
 
   wall.position = new BABYLON.Vector3(0.5 * (u.x + v.x), 0.5 * height, 0.5 * (u.y + v.y));
